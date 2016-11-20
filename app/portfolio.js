919bccba4fe7eefcecc14a5098ff83f9fda4b2a6
Alice <alicescfernandes@gmail.com>
2016-11-20 21:47:02 +0000

    first upload

---
+++ app/portfolio.js
@@ -0,0 +1,59 @@
+function getProjects(){
+
+	return( [
+				
+				{
+					"nome":"car_express",
+					"tag":["branding"],
+					"tipo":"img",
+					"length":"1",
+					"color":"#E8E5E2",
+					"title":"Car Express",
+					"sumario":"Exercicio para desenvolver marca incluindo mini-kit de normas e desdobrável"
+
+				},
+				{
+					"nome":"maasai",
+					"tag":["branding"],
+					"tipo":"img",
+					"length":"1",
+					"color":"white",
+					"title":"Maasai",
+					"sumario":"Desenvolvimento de marca para um negócio de comércio de joias. A marca foi baseada na tribo Maasai"
+
+				},
+				{
+					"nome":"unicuidar",
+					"tag":["branding"],
+					"tipo":"img",
+					"length":"1",
+					"color":"white",
+					"title":"UniCuidar",
+					"sumario":"Desenvolvimento de marca para serviço particular de enfermagem"
+
+				},
+				{
+					"nome":"diga",
+					"tag":["branding"],
+					"tipo":"img",
+					"length":"1",
+					"color":"white",
+					"title":"Diga que vai Daqui",
+					"sumario":"Desenvolvimento de marca para um blogue sobre viagens e turismo"
+
+				},
+				{
+					"nome":"comingsoon",
+					"tag":["programacao","uxui", "mobile"],
+					"tipo":"img",
+					"length":"1",
+					"color":"#111010",
+					"title":"Schedule App",
+					"sumario":"Desenvolvimento de app para android onde é possivel consultar o horário num período de 7 dias, também com informações da sala e formador da respetiva sessão.",
+					"urls":{"Github":"https://github.com/alicescfernandes/course-schedule-app"}
+
+				},
+	]
+)
+	
+}
