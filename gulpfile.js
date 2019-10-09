0ada1f73728a958bfecd4d63be17d2043b93e1b3
Alice Fernandes <alicescfernandes@gmail.com>
2019-10-09 01:26:26 +0100

    new files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const webp = require('gulp-webp');
 
 sass.compiler = require('node-sass');
 
@@ -9,10 +10,17 @@ const autoPrefixerOptions = {
     cascade:false
 }
 gulp.task('default', function () {
-    return gulp.src('./scss/**/*.scss')
+    return gulp.src('./src/scss/**/*.scss')
     .pipe(autoprefixer(autoPrefixerOptions))
         .pipe(sass(sassOptions).on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
+
+
+gulp.task('images', () =>
+    gulp.src('src/img/*')
+        .pipe(webp())
+        .pipe(gulp.dest('./img'))
+);
 
 gulp.watch(['./scss/**/*.scss'], gulp.parallel(["default"]));
