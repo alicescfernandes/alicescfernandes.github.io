a90850177b9b9274a065326b266362fcdb5658fa
Alice Fernandes <alicescfernandes@gmail.com>
2019-10-09 01:04:29 +0100

    files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 sass.compiler = require('node-sass');
 
-const sassOptions = {outputStyle: 'compressed'}
+const sassOptions = {outputStyle: 'compressed'};
+const autoPrefixerOptions = {
+    cascade:false
+}
 gulp.task('default', function () {
     return gulp.src('./scss/**/*.scss')
+    .pipe(autoprefixer(autoPrefixerOptions))
         .pipe(sass(sassOptions).on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
